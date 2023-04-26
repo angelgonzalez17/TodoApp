c1a9728bc5fd79be29fe9eb56b05b40b6ad7ed92
angelgonzalez17 <gonzalezchaconangel@gmail.com>
2023-04-26 18:10:27 -0400

    clase 03 lista

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
-    const task = document.querySelector("[data-task");
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement('li');
+    task.classList.add('card');
     input.value = '';
     const content=  `<div>
     <i class="far fa-check-square icon"></i>
     <span class="task">${value}</span>
   </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
-    console.log(content);
-
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+  list.appendChild(task);
+  console.log(content);
 };
+
 console.log(btn);
 
 btn.addEventListener("click",  createTask);
